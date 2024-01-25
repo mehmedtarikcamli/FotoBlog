a9cb5ef1a6c037c938eff6214d3ce39225b056d5
Mehmed Tarık Çamlı <mehmedtarkc@gmail.com>
2024-01-25 23:23:59 +0300

    FotoBlog-Mvc

--- FotoBlog/Controllers/GonderilerController.cs
+++ FotoBlog/Controllers/GonderilerController.cs
@@ -1,6 +1,7 @@
 ﻿using FotoBlog.Data;
 using FotoBlog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FotoBlog.Controllers
 {
@@ -50,6 +51,103 @@ namespace FotoBlog.Controllers
 			}
 
 			return View();
+		}
+
+		public IActionResult Duzenle(int id)
+		{
+			var gonderi = _db.Gonderiler.Find(id);
+
+			if (gonderi == null)
+			{
+				return NotFound();
+			}
+
+			return View(gonderi);
+		}
+
+		[HttpPost]
+		public IActionResult Duzenle(int id, Gonderi gonderi)
+		{
+			if (id != gonderi.Id)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+                    var mevcutGonderi = _db.Gonderiler.Find(id);
+
+                    string eskiResimYolu = Path.Combine(_env.WebRootPath, "img", "upload", mevcutGonderi.ResimYolu);
+
+                    string yeniResimYolu = Path.Combine(_env.WebRootPath, "img", "upload", gonderi.ResimYolu);
+
+                    if (eskiResimYolu != yeniResimYolu)
+                    {
+                        System.IO.File.Delete(eskiResimYolu);
+                    }
+
+                    mevcutGonderi.Baslik = gonderi.Baslik;
+                    mevcutGonderi.ResimYolu = gonderi.ResimYolu;
+
+                    _db.Update(mevcutGonderi);
+					_db.SaveChanges();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!GonderiVarMi(id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+
+				return RedirectToAction("Index", "Home");
+			}
+
+			return View(gonderi);
+		}
+
+		private bool GonderiVarMi(int id)
+		{
+			return _db.Gonderiler.Any(e => e.Id == id);
+		}
+
+		public IActionResult Sil(int id)
+		{
+			var gonderi = _db.Gonderiler.Find(id);
+
+			if (gonderi == null)
+			{
+				return NotFound();
+			}
+
+			return View(gonderi);
+		}
+
+		[HttpPost, ActionName("SilOnaylandi")]
+		public IActionResult SilOnaylandi(int id)
+		{
+			
+			var gonderi = _db.Gonderiler.Find(id);
+
+			if (gonderi == null)
+			{
+				return NotFound();
+			}
+
+			string silinecekYol = Path.Combine(_env.WebRootPath, "img", "upload", gonderi.ResimYolu);
+
+            System.IO.File.Delete(silinecekYol);
+
+            _db.Gonderiler.Remove(gonderi);
+			_db.SaveChanges();
+
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
